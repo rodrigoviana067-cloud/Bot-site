414155742df3196bce3a5ec9c33eb1c975742025
Rodrigo <rodrigoviana067@gmail.com>
2026-08-19 20:43:56 -0300

    Fix: PORT dinâmico para Railway

--- app.py
+++ app.py
@@ -1041,7 +1041,7 @@ if __name__ == '__main__':
 
     app.run(
         host='0.0.0.0',
-        port=5001,
+        port=int(os.environ.get("PORT", 5002)),
         debug=False,
         use_reloader=False,
         threaded=True
