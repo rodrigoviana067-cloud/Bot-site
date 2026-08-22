5e2f951d75b6d0671550226604589cc1b9be7877
Rodrigo <rodrigoviana067@gmail.com>
2026-08-22 19:35:08 -0300

    Remove start.sh do Termux

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Instala dependências da Bridge (se necessário)
-npm install --production
-
-# Inicia a Bridge em background
-node server.js &
-
-# Inicia o Flask (processo principal)
-python app.py
